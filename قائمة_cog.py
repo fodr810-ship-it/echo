504fac77d67f4f74cc49b706139eb1f5b46ef799
fodr810-ship-it <fodr810@gmail.com>
2026-06-28 22:06:39 +0000

    Fix indentation in custom avatar cog

--- قائمة_cog.py
+++ قائمة_cog.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 class GamesListCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        # حط آيدي الروم المخصص للألعاب هنا (إذا تبي الأمر يشتغل بروم واحد)
         self.allowed_channel_id = 1518382443000893440 
 
     @commands.Cog.listener()
@@ -12,58 +11,32 @@ class GamesListCog(commands.Cog):
         if message.author.bot:
             return
             
-        # (اختياري) إذا تبي الأمر يشتغل في روم محدد بس، شيل علامة المربع من السطرين اللي تحت
+        # إذا أردت تفعيل حصر الروم، قم بإزالة علامات #
         # if message.channel.id != self.allowed_channel_id:
         #     return
             
         if message.content.strip() == "العاب":
             
-            # ترتيبك الأنيق للألعاب تم وضعه هنا
+            # تم إضافة \n إضافية للفصل بين القسمين وتحسين المظهر
             games_text = (
-                "**solo ⟢**\n"
-                "𓂃 ترتيب\n"
-                "𓂃 حروف\n"
-                "𓂃 ايموجي\n"
-                "𓂃 اسرع\n"
-                "𓂃 فكك\n"
-                "𓂃 اشبك\n"
-                "𓂃 صحح\n"
-                "𓂃 اعلام\n"
-                "𓂃 قارة\n"
-                "𓂃 عواصم\n"
-                "𓂃 مفرد\n"
-                "𓂃 جمع\n"
-                "𓂃 اكشف\n"
-                "𓂃 الوان\n"
-                "𓂃 زر\n"
-                "𓂃 ارقام\n"
-                "𓂃 !حجرة\n"
-                "𓂃 !xo\n"
-                "𓂃 حساب"
-
-               "----------------------------------"
-               
+                "**Solo ⟢**\n"
+                "𓂃 ترتيب\n𓂃 حروف\n𓂃 ايموجي\n𓂃 اسرع\n𓂃 فكك\n"
+                "𓂃 اشبك\n𓂃 صحح\n𓂃 اعلام\n𓂃 قارة\n𓂃 عواصم\n"
+                "𓂃 مفرد\n𓂃 جمع\n𓂃 اكشف\n𓂃 الوان\n𓂃 زر\n"
+                "𓂃 ارقام\n𓂃 !حجرة\n𓂃 !xo\n𓂃 حساب\n\n"
                 "**Group ⟢**\n"
-                "𓂃 كراسي!\n"
-                "𓂃 مافيا!\n"
-                "𓂃 روليت!\n"
-                "𓂃 !codenames\n"
-                "𓂃 غميضة!\n"
-                "𓂃 ريبلكا!\n"
-
+                "𓂃 كراسي!\n𓂃 مافيا!\n𓂃 روليت!\n𓂃 !codenames\n"
+                "𓂃 غميضة!\n𓂃 ريبلكا!"
             )
 
-            # إنشاء الإمبد
             embed = discord.Embed(
                 description=games_text,
                 color=discord.Color.blurple()
             )
             
-            # صورة البوت تطلع على اليمين فوق
             if self.bot.user.display_avatar:
                 embed.set_thumbnail(url=self.bot.user.display_avatar.url)
             
-            # فوتر خفيف يعلمهم كيف يشوفون نقاطهم
             embed.set_footer(text="EchoGamesSystem")
             
             await message.channel.send(embed=embed)
